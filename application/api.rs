08179ce06b343ffe0a554d1b0236481501fb684f
David Steiner <david_j_steiner@yahoo.co.nz>
2023-04-15 22:49:53 +0200

    Move routes into the api module

--- application/api.rs
+++ application/api.rs
@@ -1,5 +1,41 @@
-pub mod download;
-pub mod index;
-pub mod publish;
-pub mod unyank;
-pub mod yank;
+mod download;
+mod index;
+mod publish;
+mod unyank;
+mod yank;
+
+use axum::routing::{delete, get, put, Router};
+
+use crate::api::download::download_crate;
+use crate::api::index::{
+    get_info_for_long_name_crate, get_info_for_short_name_crate, get_info_for_three_letter_crate,
+};
+use crate::api::publish::publish_crate;
+use crate::api::unyank::unyank;
+use crate::api::yank::yank;
+use crate::app_state::AppState;
+use crate::get_config_json;
+use crate::repository::Repository;
+use crate::storage::CrateStorage;
+
+pub fn build_router<R: Repository, S: CrateStorage>() -> Router<AppState<R, S>> {
+    Router::new()
+        .route("/config.json", get(get_config_json))
+        .route("/api/v1/crates/new", put(publish_crate))
+        .route("/api/v1/crates/:crate_name/:version/yank", delete(yank))
+        .route("/api/v1/crates/:crate_name/:version/unyank", put(unyank))
+        .route(
+            "/api/v1/crates/:crate_name/:version/download",
+            get(download_crate),
+        )
+        .route("/1/:crate_name", get(get_info_for_short_name_crate))
+        .route("/2/:crate_name", get(get_info_for_short_name_crate))
+        .route(
+            "/3/:first_letter/:crate_name",
+            get(get_info_for_three_letter_crate),
+        )
+        .route(
+            "/:first_two/:second_two/:crate_name",
+            get(get_info_for_long_name_crate),
+        )
+}
--- application/main.rs
+++ application/main.rs
@@ -5,20 +5,13 @@ pub mod models;
 pub mod repository;
 pub mod storage;
 
+use crate::api::build_router;
 use aws_sdk_dynamodb::Client;
 use axum::http::StatusCode;
-use axum::routing::{delete, get, put};
 use axum::{Json, Router};
 use lambda_web::run_hyper_on_lambda;
 use serde::Serialize;
 
-use crate::api::download::download_crate;
-use crate::api::index::{
-    get_info_for_long_name_crate, get_info_for_short_name_crate, get_info_for_three_letter_crate,
-};
-use crate::api::publish::publish_crate;
-use crate::api::unyank::unyank;
-use crate::api::yank::yank;
 use crate::app_state::AppState;
 use crate::repository::DynamoDBRepository;
 use crate::storage::S3Storage;
@@ -51,26 +44,7 @@ async fn main() {
         storage,
     };
 
-    let app = Router::new()
-        .route("/config.json", get(get_config_json))
-        .route("/api/v1/crates/new", put(publish_crate))
-        .route("/api/v1/crates/:crate_name/:version/yank", delete(yank))
-        .route("/api/v1/crates/:crate_name/:version/unyank", put(unyank))
-        .route(
-            "/api/v1/crates/:crate_name/:version/download",
-            get(download_crate),
-        )
-        .route("/1/:crate_name", get(get_info_for_short_name_crate))
-        .route("/2/:crate_name", get(get_info_for_short_name_crate))
-        .route(
-            "/3/:first_letter/:crate_name",
-            get(get_info_for_three_letter_crate),
-        )
-        .route(
-            "/:first_two/:second_two/:crate_name",
-            get(get_info_for_long_name_crate),
-        )
-        .with_state(app_state);
+    let app = build_router().with_state(app_state);
 
     run_app(app).await
 }
